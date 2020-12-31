28099856c6e4db6a3863751f3cdcb9063621ea21
Rafael Magalhaes <rafalm.lb@gmail.com>
2020-12-30 21:00:25 -0300

    feat: ajuste entity Activity e Calendar

--- src/main/java/com/fiap/chat/entity/Activity.java
+++ src/main/java/com/fiap/chat/entity/Activity.java
@@ -1,10 +1,19 @@
 package com.fiap.chat.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+//import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.Valid;
 
 import lombok.Data;
 
@@ -14,5 +23,22 @@ import lombok.Data;
 public class Activity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="act_ID")
 	private Long id;
+	
+	@Column(name="descricao")
+	private String descricao;
+	
+	@Column(name="titulo")
+	private String titulo;
+	
+	@Column(name="datetime")
+	private LocalDateTime datetime;
+	
+	@ManyToOne(cascade = CascadeType.ALL)//,  orphanRemoval=true)
+	@JoinColumn(name="cal_ID")
+	private Calendar calendar;
+	
+	
+	
 }
--- src/main/java/com/fiap/chat/entity/Calendar.java
+++ src/main/java/com/fiap/chat/entity/Calendar.java
@@ -1,30 +1,61 @@
 package com.fiap.chat.entity;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+//import javax.persistence.Column;
+//import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+//import javax.validation.Valid;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+//import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.Data;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Table(name="tb_calendar")
 public class Calendar {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="cal_ID")
 	private Long id;
-	@ManyToOne(fetch = FetchType.LAZY)
+	
+	@OneToOne(fetch = FetchType.LAZY)
 	private User user;
+	
+	@OneToMany(cascade = CascadeType.ALL)//, mappedBy = "calendar")//, orphanRemoval=true)
+	@JoinColumn(name = "act_ID")
+	private List<Activity> activity;
+	
+	/*
+	@Embedded
+	@Valid
+	@OneToOne(fetch = FetchType.LAZY)
+	private User user;
+	
 	//@OneToMany(cascade = CascadeType.ALL)
 	//private List<Activity> activities;
 	//private List<LocalDate> dates;
+	@Embedded
+	@Valid
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Activity> activity;
+	//private List<LocalDate> diasNaoUteis;
+	 */
 }
